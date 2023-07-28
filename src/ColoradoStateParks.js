eb71d56ecee0349b497874bbb5daeaa5a444fcd5
gloria <gloria.gao115@gmail.com>
2023-07-29 00:15:00 +1000

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees as parkTrees, wildlife as parkWildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(parkTrees)
+  console.log(RMFunctions.trees)
+  parkWildlife()
+  RMFunctions.wildlife()
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks
